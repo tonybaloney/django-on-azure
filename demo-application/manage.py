257c81f0b41d1d547fb7beccb27d206f71cdb146
Anthony Shaw <anthony.p.shaw@gmail.com>
2022-08-11 10:20:55 +1000

    Switch to OTEL

--- demo-application/manage.py
+++ demo-application/manage.py
@@ -2,12 +2,26 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from opentelemetry.instrumentation.django import DjangoInstrumentor
+from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
+from opentelemetry.exporter.richconsole import RichConsoleExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+    os.environ.setdefault('WEBSITE_HOSTNAME', 'localhost')
     try:
+        tracer = TracerProvider(resource=Resource({SERVICE_NAME: "FastAPI"}))
+        tracer.add_span_processor(BatchSpanProcessor(RichConsoleExporter()))
+        tracer.add_span_processor(BatchSpanProcessor(
+            AzureMonitorTraceExporter.from_connection_string(
+                os.getenv('APPLICATIONINSIGHTS_CONNECTION_STRING')
+            )
+        ))
+        DjangoInstrumentor().instrument(tracer_provider = tracer)
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
